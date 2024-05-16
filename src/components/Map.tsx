7bc13264ded1b942b50ae8ee0d1678e16d6d8dc3
eKhafizov <130173250+eKhafizov@users.noreply.github.com>
2024-05-16 21:07:48 +0300

    added comments

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,56 +6,62 @@ import {Icon, Marker, layerGroup} from 'leaflet';
 
 
 //markers
-export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
-export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
 const defaultCustomIcon = new Icon({
-  iconUrl: URL_MARKER_DEFAULT,
+  iconUrl: 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40]
 });
 //const currentCustomIcon = new Icon({
-  //iconUrl: URL_MARKER_CURRENT,
+  //iconUrl: 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg',
   //iconSize: [40, 40],
   //iconAnchor: [20, 40]
 //});
 
 
 function Map(props: AppType): JSX.Element {
+  //используем хук useRef и вешаем ссылку на DOM дива, где будет отрисована карта
   const myRef = useRef(null);
+
   // создаем переменную map - используя хук useMap, который строит карту
   // в этот хук передаем город из props.city и ссылку на объект DOM, куда эту карту отрисуем
   const map = useMap(myRef, props.city);
+
+  //переменная points = переданному props.offers(передаем все наши объекты)
   const points = props.offers;
 
+  //используем хук useEffect, чтобы добавлять маркеры на отрисованную карту
   useEffect(() => {
     if (map) {
+      //создаем слой
       const markerLayer = layerGroup().addTo(map);
+      //для каждого объекта из props.offers делаем маркер
       points.forEach((point) => {
         const marker = new Marker({
           lat: point.lat,
           lng: point.lng
         });
-
+        //добавляем маркеру иконку
         marker
           .setIcon(defaultCustomIcon
           )
+        //добавляем маркер на слой
           .addTo(markerLayer);
       });
 
+      //убираем слой перед размонтированием элемента, он добавится после обновления при монтировании
       return () => {
         map.removeLayer(markerLayer);
       };
     }
   }, [map, points]);
 
+
   return (
     <div className="cities__right-section">
       <section className="cities__map map">
         <div
           style={{height: '600px'}}
-          ref={myRef}
+          ref={myRef} //добавили ссылку на элемент DOM с картой
         >
         </div>
       </section>
