b21cf01976ebea16c765d70d1c43f55514695ba9
eKhafizov <130173250+eKhafizov@users.noreply.github.com>
2024-08-04 07:23:40 +0300

    xxx

---
+++ src/components/PlacesList/styles.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import styled from 'styled-components';
+
+
+const X = styled.button`
+  color: ${(props) => (props.theme.fontColorBlack)};
+`;
+
+export default X;
